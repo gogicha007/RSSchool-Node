7f2c147937bbe2174b686a23a17a573be8e72906
gogicha007 <gogicha@gmail.com>
2025-04-23 15:32:47 +0400

    chor: graphql

---
+++ node-task-graphql/src/routes/graphql/index.ts
@@ -0,0 +1,48 @@
+import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import { createGqlResponseSchema, gqlResponseSchema, Schema } from './schemas.js';
+import { graphql, validate, GraphQLArgs, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+import { createDataLoaders } from './dataLoaders.js';
+
+const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
+  const dataLoaders = createDataLoaders(prisma);
+
+  fastify.route({
+    url: '/',
+    method: 'POST',
+    schema: {
+      ...createGqlResponseSchema,
+      response: {
+        200: gqlResponseSchema,
+      },
+    },
+    async handler(req) {
+      const gqlErrs = validate(Schema, parse(req.body.query), [depthLimit(5)], {
+        maxErrors: 1,
+      });
+      if (gqlErrs.length > 0) {
+        return { errors: gqlErrs };
+      }
+
+      const gqlArgs: GraphQLArgs = {
+        schema: Schema,
+        source: req.body.query,
+        contextValue: { prisma: prisma, ...dataLoaders },
+      };
+
+      if (req.body.variables) {
+        gqlArgs.variableValues = req.body.variables;
+      }
+      try {
+        const result = await graphql(gqlArgs);
+        return result;
+      } catch (error) {
+        return { errors: [error] };
+      }
+    },
+  });
+};
+
+export default plugin;
